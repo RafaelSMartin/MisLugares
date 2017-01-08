593ac9b666e946c90107e8eb982404f5aca042d6
RafaelSMartin <rafaels.martin.dev@gmail.com>
2017-01-08 11:58:27 +0100

    Menu contextual About y Main menu

---
+++ app/src/main/java/com/rafaels/mislugares/About.java
@@ -0,0 +1,17 @@
+package com.rafaels.mislugares;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by Rafel S. Martin on 08/01/2017.
+ */
+
+public class About extends Activity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.about);
+    }
+}
--- app/src/main/java/com/rafaels/mislugares/MainActivity.java
+++ app/src/main/java/com/rafaels/mislugares/MainActivity.java
@@ -1,13 +1,48 @@
 package com.rafaels.mislugares;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+    }
+
+    public void lanzarAbout(View view){
+        Intent i = new Intent(this, About.class);
+        startActivity(i);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+        //super.onCreateOptionsMenu(menu);
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item){
+        int id = item.getItemId();
+        if(id == R.id.config){
+            return true;
+        }
+        if(id == R.id.about){
+            lanzarAbout(null);
+            return true;
+        }
+        if(id == R.id.search){
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
